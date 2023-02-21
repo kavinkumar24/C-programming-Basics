c953f6d4a6870a2a6cf40f47f6289f7052f6cf8d
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-02-21 22:23:20 +0530

    Update problem3.C

--- codechef_Answers/problem3.C
+++ codechef_Answers/problem3.C
@@ -1,6 +1,19 @@
+
+///////Discount////////////////
+/*Problem
+Alice buys a toy with a selling price of 
+100
+100 rupees. There is a discount of 
+x percent on the toy. Find the amount Alice needs to pay for it.
+Input Format
+The first line of input will contain a single integer 
+T, denoting the number of test cases.
+The first and only line of each test case contains a single integer, 
+x — the discount on the toy.
+*/
+
 #include <stdio.h>
 int main(void) {
-    
     int t;
     scanf("%d",&t);
     for(int i=0;i<t;i++){

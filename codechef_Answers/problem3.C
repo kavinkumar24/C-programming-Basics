c4caf0b4884368c5f6ce9b462fb4d57fb7b1b4e3
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-02-21 22:22:23 +0530

    Update problem3.C

--- codechef_Answers/problem3.C
+++ codechef_Answers/problem3.C
@@ -1,2 +1,13 @@
 #include <stdio.h>
-
+int main(void) {
+    
+    int t;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++){
+        int a;
+        scanf("%d \n",&a);
+        int c = 100-a;
+        printf("%d \n",c);
+    }
+	return 0;
+}

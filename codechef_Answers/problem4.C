7ea4cc8741126f040a0399d07535a4037ea0a675
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-02-24 21:31:08 +0530

    Update problem4.C

--- codechef_Answers/problem4.C
+++ codechef_Answers/problem4.C
@@ -6,3 +6,23 @@
 Jerry is running at a speed of X metres per second while Tom is chasing him at a speed of Y metres per second. Determine whether Tom will be able to catch Jerry.
  Note that initially Jerry is not at the same position as Tom.*/
 
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    scanf("%d",&i);
+    for(int n=0;n<i;n++){
+        int x,y;
+        scanf("%d %d",&x,&y);
+        if(y>x){
+            printf("YES \n");
+        }
+        else{
+            printf("NO \n");
+        }
+    }
+	return 0;
+}
+
+
+

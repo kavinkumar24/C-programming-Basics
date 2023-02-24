6d3a0ca66dd95e0eb178ae324e2c07eb4f960677
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-02-24 21:30:48 +0530

    Create problem4.C

---
+++ codechef_Answers/problem4.C
@@ -0,0 +1,8 @@
+
+
+///////////////////////////////////Tom and Jerry Chase///////////////////////////////////////
+///Problem
+/*In a classic chase, Tom is running after Jerry as Jerry has eaten Tom's favourite food.
+Jerry is running at a speed of X metres per second while Tom is chasing him at a speed of Y metres per second. Determine whether Tom will be able to catch Jerry.
+ Note that initially Jerry is not at the same position as Tom.*/
+

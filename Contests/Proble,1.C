f4c9634815de3e7929c6ff2b99dff29f34c36fbf
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-08 22:30:48 +0530

    Create Proble,1.C

---
+++ Contests/Proble,1.C
@@ -0,0 +1,10 @@
+///////////////////////Just One More Episode////////////////////////////
+
+
+/*Problem
+Chef has to attend an exam that starts in X minutes, but of course, watching shows takes priority. Every episode of the show that Chef is watching, is  24
+24 minutes long.
+If he starts watching a new episode now, will he finish watching it strictly before the exam starts?*/
+
+///////////////////////////////SOLUTION//////////////////////////
+

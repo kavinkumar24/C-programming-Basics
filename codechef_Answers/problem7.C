3460f8c6e42ac8ee4a4a4f0ce45403e9e129e341
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-10 22:48:17 +0530

    Update problem7.C

--- codechef_Answers/problem7.C
+++ codechef_Answers/problem7.C
@@ -11,3 +11,20 @@ Chef defines a program as: Good, if it takes exactly
 N bits of memory, determine whether it is Good or Not Good.*/
 
 
+#include <stdio.h>
+
+int main(void) {
+	int T;
+	scanf("%d",&T);
+    for(int i = 0;i<T;i++){
+        int req;
+        scanf("%d",&req);
+        if(req%4==0){
+            printf("GOOD\n");
+        }
+        else{
+            printf("NOT GOOD\n");
+        }
+    }
+	return 0;
+}

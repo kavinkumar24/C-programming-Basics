776e48a5d50ffe5a8e640ece346bd3da78f65fe0
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-10 22:48:05 +0530

    Create problem7.C

---
+++ codechef_Answers/problem7.C
@@ -0,0 +1,13 @@
+////////////////          Good Program         /////////////////////
+
+/* Problem
+In computing, the collection of four bits is called a nibble.
+
+Chef defines a program as: Good, if it takes exactly 
+
+ X nibbles of memory, where X is a positive integer.
+  Not Good, otherwise.
+  Given a program which takes
+N bits of memory, determine whether it is Good or Not Good.*/
+
+

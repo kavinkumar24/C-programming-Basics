4b7cb920377344c166a90840b0fcd7d3bacdc39c
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-10 22:46:10 +0530

    Update problem6.C

--- codechef_Answers/problem6.C
+++ codechef_Answers/problem6.C
@@ -2,9 +2,7 @@
 
 
 /*Problem
-There are 
-2
-2 stores in Chefland and both sell the same product. The first store sells the product for 100 rupees whereas the second store sells it for 200 rupees.
+There are 2 stores in Chefland and both sell the same product. The first store sells the product for 100 rupees whereas the second store sells it for 200 rupees.
 It is the holiday season and both stores have announced a special discount. The first store is providing a discount of
 A percent on its product and the second store is providing a discount of B percent on its product.
 Chef is wondering which store is selling the product at a cheaper price after the discount has been applied. Can you help him identify the better deal*/

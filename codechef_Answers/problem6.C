bfe3b4d480214902a45f3f00a030ea98763a2ff5
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-10 22:46:23 +0530

    Update problem6.C

--- codechef_Answers/problem6.C
+++ codechef_Answers/problem6.C
@@ -6,3 +6,30 @@ There are 2 stores in Chefland and both sell the same product. The first store s
 It is the holiday season and both stores have announced a special discount. The first store is providing a discount of
 A percent on its product and the second store is providing a discount of B percent on its product.
 Chef is wondering which store is selling the product at a cheaper price after the discount has been applied. Can you help him identify the better deal*/
+
+
+#include <stdio.h>
+
+int main(void) {
+    int T;
+    scanf("%d",&T);
+    for(int i = 0;i<T;i++){
+        int A,B;
+        float C,D;
+        scanf("%d %d",&A,&B);
+        C = 100 - (100 * A/100);
+        D = 200 - (200 * B/100);
+        if(C>D){
+            printf("SECOND\n");
+        }
+        else if(C==D){
+            printf("BOTH\n");
+        }
+        else{
+            printf("FIRST\n");
+        }
+        
+    }
+	return 0;
+}
+

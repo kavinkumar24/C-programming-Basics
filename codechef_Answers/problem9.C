982d5878aaf176d3440931ac9a3040b0f36c8db6
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-12 21:38:09 +0530

    Create problem9.C

---
+++ codechef_Answers/problem9.C
@@ -0,0 +1,8 @@
+////////    Sale Season//////////
+/* Problem
+It's the sale season again and Chef bought items worth a total of 
+X rupees. The sale season offer is as follows:
+if X≤100, no discount.
+if 100<X≤1000, discount is  25 rupees.
+if 1000<X≤5000, discount is 100 rupees.
+if X>5000, discount is  500 rupees. Find the final amount Chef needs to pay for his shopping.*/

7f255928f41e0b6cf636a4de7d2d70ec96da5287
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-12 21:38:29 +0530

    Update problem9.C

--- codechef_Answers/problem9.C
+++ codechef_Answers/problem9.C
@@ -6,3 +6,7 @@ if X≤100, no discount.
 if 100<X≤1000, discount is  25 rupees.
 if 1000<X≤5000, discount is 100 rupees.
 if X>5000, discount is  500 rupees. Find the final amount Chef needs to pay for his shopping.*/
+
+
+
+///////////////solution////////////

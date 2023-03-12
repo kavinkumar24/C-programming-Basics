7fef1d7d0bf9493926faec7aa15834b00af27abf
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-12 21:35:52 +0530

    Create Problem8.C

---
+++ codechef_Answers/Problem8.C
@@ -0,0 +1,8 @@
+/////////////////            Codechef Airlines////////////////
+
+/* Problem
+Chef has opened a new airline. Chef has 
+10 airplanes where each airplane has a capacity of 
+X passengers.
+On the first day itself, Y people are willing to book a seat in any one of Chef's airplanes.
+Given that Chef charges Z rupees for each ticket, find the maximum amount he can earn on the first day.*/

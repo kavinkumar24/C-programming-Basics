7c9eb987f3eca1145752a0575d3e19de06ad9ccd
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-13 22:11:16 +0530

    Update problem10.C

--- codechef_Answers/problem10.C
+++ codechef_Answers/problem10.C
@@ -5,3 +5,20 @@
 If Give an integer N . Write a program to obtain the sum of the first and last digits of this number.*/
 
 //// solution///////////
+
+#include <stdio.h>
+
+int main()
+{
+    int l,n,t,r;
+    
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d",&n);
+        l = n%10;
+        while(n>0) {r=n%10; n/=10;}
+        printf("%d\n",r+l);
+    }
+    return 0;
+}
+

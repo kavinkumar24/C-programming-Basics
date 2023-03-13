b87b4e41910544f41022f124de05ce3527a91d32
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-03-13 22:10:48 +0530

    Create problem10.C

---
+++ codechef_Answers/problem10.C
@@ -0,0 +1,5 @@
+///////////////         First and Last Digit         /////////////////
+
+
+/*Problem
+If Give an integer N . Write a program to obtain the sum of the first and last digits of this number.*/

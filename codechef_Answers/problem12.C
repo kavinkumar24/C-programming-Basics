70f786c074e30005c1f06bcf24ebb73ff702ce7e
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-04-08 21:38:13 +0530

    Create problem12.C

---
+++ codechef_Answers/problem12.C
@@ -0,0 +1,25 @@
+// que:
+/// Couple Game///
+
+//Problem
+/*There are 
+�
+G girl and 
+�
+B boy students at IIT (BHU) such that 
+�
+>
+�
+B>G.
+
+If ICM were a team game where teams could only be of size 
+2
+2, having exactly 
+1
+1 girl student and 
+1
+1 boy student, what would be the minimum number of boy students from IIT (BHU) who would not be able to participate?*/
+
+
+
+/// ans:

1307e8b207d475b269200890622dfadd8b706d85
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-04-14 21:26:32 +0530

    Create problem14.C

---
+++ codechef_Answers/problem14.C
@@ -0,0 +1,7 @@
+/*Problem
+Chef is given three numbers A,B, and C.
+He wants to find whether he can select exactly two numbers out of these such that the product of the selected numbers is negative.*/
+
+
+//solution
+

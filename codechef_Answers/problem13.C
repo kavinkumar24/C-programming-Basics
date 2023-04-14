f321478beeb80c21a651a63d38a44df7d92f7f59
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-04-14 21:25:36 +0530

    Update problem13.C

--- codechef_Answers/problem13.C
+++ codechef_Answers/problem13.C
@@ -3,3 +3,25 @@
 
 
 
+// solution
+
+
+#include <stdio.h>
+int main(void) {
+	int t,a,b;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++){
+	    int m,n;
+	    scanf("%d %d",&m,&n);
+	    a=m;
+	    b=n;
+	    while(n!=0){
+	        int r;
+	        r=m%n;
+	        m=n;
+	        n=r;}
+        long LCM = (long int)a*b/m;
+	    printf("%d %li\n",m,LCM);
+	}
+	return 0;
+}

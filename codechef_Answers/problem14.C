cc435eff7f6791c576fd5f8887212a7919629e19
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-04-14 21:26:44 +0530

    Update problem14.C

--- codechef_Answers/problem14.C
+++ codechef_Answers/problem14.C
@@ -5,3 +5,21 @@ He wants to find whether he can select exactly two numbers out of these such tha
 
 //solution
 
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++){
+	    int a,b,c;
+	    scanf("%d %d %d",&a,&b,&c);
+	    if(a*b<0 || b*c<0 || c*a<0){
+	        printf("YES\n");
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
